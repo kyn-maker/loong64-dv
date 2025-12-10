1c7d80cf4e225d40d8e64c9978bb805bd98a4404
“kyn” <3253197618@qq.com>
2025-12-10 09:04:41 +0800

    基础浮点数指令

--- src/isa/la64f_instr.sv
+++ src/isa/la64f_instr.sv
@@ -23,8 +23,8 @@
 `DEFINE_FP_INSTR(FRSQRT_S, R2_TYPE, ARITHMETIC, LA64) // frsqrt.s fd, fj
 `DEFINE_FP_INSTR(FLOGB_S, R2_TYPE, ARITHMETIC, LA64) // flogb.s fd, fj
 `DEFINE_FP_INSTR(FCLASS_S, R2_TYPE, ARITHMETIC, LA64) // fclass.s fd, fj
-`DEFINE_FP_INSTR(FRECIPE_S, R2_TYPE, ARITHMETIC, LA64) // frecipe.s fd, fj
-`DEFINE_FP_INSTR(FRSQRTE_S, R2_TYPE, ARITHMETIC, LA64) // frsqrte.s fd, fj
+//`DEFINE_FP_INSTR(FRECIPE_S, R2_TYPE, ARITHMETIC, LA64) // frecipe.s fd, fj
+//`DEFINE_FP_INSTR(FRSQRTE_S, R2_TYPE, ARITHMETIC, LA64) // frsqrte.s fd, fj
 
 // 单精度浮点乘加/乘减指令：fd, fj, fk, fa (4个浮点寄存器，使用R4_TYPE格式)
 `DEFINE_FP_INSTR(FMADD_S, R4_TYPE, ARITHMETIC, LA64)  // fmadd.s fd, fj, fk, fa
@@ -52,11 +52,119 @@
 `DEFINE_FP_INSTR(FRSQRT_D, R2_TYPE, ARITHMETIC, LA64) // frsqrt.d fd, fj
 `DEFINE_FP_INSTR(FLOGB_D, R2_TYPE, ARITHMETIC, LA64) // flogb.d fd, fj
 `DEFINE_FP_INSTR(FCLASS_D, R2_TYPE, ARITHMETIC, LA64) // fclass.d fd, fj
-`DEFINE_FP_INSTR(FRECIPE_D, R2_TYPE, ARITHMETIC, LA64) // frecipe.d fd, fj
-`DEFINE_FP_INSTR(FRSQRTE_D, R2_TYPE, ARITHMETIC, LA64) // frsqrte.d fd, fj
+//`DEFINE_FP_INSTR(FRECIPE_D, R2_TYPE, ARITHMETIC, LA64) // frecipe.d fd, fj
+//`DEFINE_FP_INSTR(FRSQRTE_D, R2_TYPE, ARITHMETIC, LA64) // frsqrte.d fd, fj
 
 // 双精度浮点乘加/乘减指令：fd, fj, fk, fa (4个浮点寄存器，使用R4_TYPE格式)
 `DEFINE_FP_INSTR(FMADD_D, R4_TYPE, ARITHMETIC, LA64)  // fmadd.d fd, fj, fk, fa
 `DEFINE_FP_INSTR(FMSUB_D, R4_TYPE, ARITHMETIC, LA64)  // fmsub.d fd, fj, fk, fa
 `DEFINE_FP_INSTR(FNMADD_D, R4_TYPE, ARITHMETIC, LA64) // fnmadd.d fd, fj, fk, fa
 `DEFINE_FP_INSTR(FNMSUB_D, R4_TYPE, ARITHMETIC, LA64) // fnmsub.d fd, fj, fk, fa
+
+// 浮点转换指令：fd, fj (2个浮点寄存器，使用R2_TYPE格式)
+`DEFINE_FP_INSTR(FCVT_S_D, R2_TYPE, ARITHMETIC, LA64)  // fcvt.s.d fd, fj
+`DEFINE_FP_INSTR(FCVT_D_S, R2_TYPE, ARITHMETIC, LA64)  // fcvt.d.s fd, fj
+// 浮点到整数转换指令：fd, fj (2个浮点寄存器，使用R2_TYPE格式)
+`DEFINE_FP_INSTR(FFINT_S_W, R2_TYPE, ARITHMETIC, LA64)  // ffint.s.w fd, fj
+`DEFINE_FP_INSTR(FFINT_S_L, R2_TYPE, ARITHMETIC, LA64)  // ffint.s.l fd, fj
+`DEFINE_FP_INSTR(FFINT_D_W, R2_TYPE, ARITHMETIC, LA64)  // ffint.d.w fd, fj
+`DEFINE_FP_INSTR(FFINT_D_L, R2_TYPE, ARITHMETIC, LA64)  // ffint.d.l fd, fj
+// 整数到浮点转换指令：fd, fj (2个浮点寄存器，使用R2_TYPE格式)
+`DEFINE_FP_INSTR(FTINT_W_S, R2_TYPE, ARITHMETIC, LA64)  // ftint.w.s fd, fj
+`DEFINE_FP_INSTR(FTINT_L_S, R2_TYPE, ARITHMETIC, LA64)  // ftint.l.s fd, fj
+`DEFINE_FP_INSTR(FTINT_W_D, R2_TYPE, ARITHMETIC, LA64)  // ftint.w.d fd, fj
+`DEFINE_FP_INSTR(FTINT_L_D, R2_TYPE, ARITHMETIC, LA64)  // ftint.l.d fd, fj
+// 带舍入模式的浮点到整数转换指令：fd, fj (2个浮点寄存器，使用R2_TYPE格式)
+// RM: Round to Minus Infinity (向负无穷舍入)
+`DEFINE_FP_INSTR(FTINTRM_W_S, R2_TYPE, ARITHMETIC, LA64)  // ftintrm.w.s fd, fj
+`DEFINE_FP_INSTR(FTINTRM_W_D, R2_TYPE, ARITHMETIC, LA64)  // ftintrm.w.d fd, fj
+`DEFINE_FP_INSTR(FTINTRM_L_S, R2_TYPE, ARITHMETIC, LA64)  // ftintrm.l.s fd, fj
+`DEFINE_FP_INSTR(FTINTRM_L_D, R2_TYPE, ARITHMETIC, LA64)  // ftintrm.l.d fd, fj
+// RP: Round to Plus Infinity (向正无穷舍入)
+`DEFINE_FP_INSTR(FTINTRP_W_S, R2_TYPE, ARITHMETIC, LA64)  // ftintrp.w.s fd, fj
+`DEFINE_FP_INSTR(FTINTRP_W_D, R2_TYPE, ARITHMETIC, LA64)  // ftintrp.w.d fd, fj
+`DEFINE_FP_INSTR(FTINTRP_L_S, R2_TYPE, ARITHMETIC, LA64)  // ftintrp.l.s fd, fj
+`DEFINE_FP_INSTR(FTINTRP_L_D, R2_TYPE, ARITHMETIC, LA64)  // ftintrp.l.d fd, fj
+// RZ: Round to Zero (向零舍入)
+`DEFINE_FP_INSTR(FTINTRZ_W_S, R2_TYPE, ARITHMETIC, LA64)  // ftintrz.w.s fd, fj
+`DEFINE_FP_INSTR(FTINTRZ_W_D, R2_TYPE, ARITHMETIC, LA64)  // ftintrz.w.d fd, fj
+`DEFINE_FP_INSTR(FTINTRZ_L_S, R2_TYPE, ARITHMETIC, LA64)  // ftintrz.l.s fd, fj
+`DEFINE_FP_INSTR(FTINTRZ_L_D, R2_TYPE, ARITHMETIC, LA64)  // ftintrz.l.d fd, fj
+// RNE: Round to Nearest Even (向最近偶数舍入)
+`DEFINE_FP_INSTR(FTINTRNE_W_S, R2_TYPE, ARITHMETIC, LA64)  // ftintrne.w.s fd, fj
+`DEFINE_FP_INSTR(FTINTRNE_W_D, R2_TYPE, ARITHMETIC, LA64)  // ftintrne.w.d fd, fj
+`DEFINE_FP_INSTR(FTINTRNE_L_S, R2_TYPE, ARITHMETIC, LA64)  // ftintrne.l.s fd, fj
+`DEFINE_FP_INSTR(FTINTRNE_L_D, R2_TYPE, ARITHMETIC, LA64)  // ftintrne.l.d fd, fj
+// 浮点舍入到整数指令：fd, fj (2个浮点寄存器，使用R2_TYPE格式)
+`DEFINE_FP_INSTR(FRINT_S, R2_TYPE, ARITHMETIC, LA64)  // frint.s fd, fj
+`DEFINE_FP_INSTR(FRINT_D, R2_TYPE, ARITHMETIC, LA64)  // frint.d fd, fj
+// 浮点移动指令：fd, fj (2个浮点寄存器，使用R2_TYPE格式)
+`DEFINE_FP_INSTR(FMOV_S, R2_TYPE, ARITHMETIC, LA64)  // fmov.s fd, fj
+`DEFINE_FP_INSTR(FMOV_D, R2_TYPE, ARITHMETIC, LA64)  // fmov.d fd, fj
+// 浮点选择指令：fd, fj, fk, ca (3个浮点寄存器 + 条件标志寄存器，使用R4_TYPE格式)
+`DEFINE_FP_INSTR(FSEL, R4_TYPE, ARITHMETIC, LA64)  // fsel fd, fj, fk, ca
+
+// GPR到FPR移动指令：fd, rj (FPR目标，GPR源，使用R2_TYPE格式)
+`DEFINE_FP_INSTR(MOVGR2FR_W, R2_TYPE, ARITHMETIC, LA64)  // movgr2fr.w fd, rj
+`DEFINE_FP_INSTR(MOVGR2FRH_W, R2_TYPE, ARITHMETIC, LA64)  // movgr2frh.w fd, rj
+`DEFINE_FP_INSTR(MOVGR2FR_D, R2_TYPE, ARITHMETIC, LA64)  // movgr2fr.d fd, rj
+
+// FPR到GPR移动指令：rd, fj (GPR目标，FPR源，使用R2_TYPE格式)
+`DEFINE_FP_INSTR(MOVFR2GR_S, R2_TYPE, ARITHMETIC, LA64)  // movfr2gr.s rd, fj
+`DEFINE_FP_INSTR(MOVFR2GR_D, R2_TYPE, ARITHMETIC, LA64)  // movfr2gr.d rd, fj
+`DEFINE_FP_INSTR(MOVFRH2GR_S, R2_TYPE, ARITHMETIC, LA64)  // movfrh2gr.s rd, fj
+
+// GPR到FCSR移动指令：fcsr, rj (FCSR目标，GPR源，使用R2_TYPE格式)
+`DEFINE_FP_INSTR(MOVGR2FCSR, R2_TYPE, ARITHMETIC, LA64)  // movgr2fcsr fcsr, rj
+
+// FCSR到GPR移动指令：rd, fcsr (GPR目标，FCSR源，使用R2_TYPE格式)
+`DEFINE_FP_INSTR(MOVFCSR2GR, R2_TYPE, ARITHMETIC, LA64)  // movfcsr2gr rd, fcsr
+
+// FPR到CF移动指令：cd, fj (CF目标，FPR源，使用R2_TYPE格式)
+`DEFINE_FP_INSTR(MOVFR2CF, R2_TYPE, ARITHMETIC, LA64)  // movfr2cf cd, fj
+
+// CF到FPR移动指令：fd, cj (FPR目标，CF源，使用R2_TYPE格式)
+`DEFINE_FP_INSTR(MOVCF2FR, R2_TYPE, ARITHMETIC, LA64)  // movcf2fr fd, cj
+
+// GPR到CF移动指令：cd, rj (CF目标，GPR源，使用R2_TYPE格式)
+`DEFINE_FP_INSTR(MOVGR2CF, R2_TYPE, ARITHMETIC, LA64)  // movgr2cf cd, rj
+
+// CF到GPR移动指令：rd, cj (GPR目标，CF源，使用R2_TYPE格式)
+`DEFINE_FP_INSTR(MOVCF2GR, R2_TYPE, ARITHMETIC, LA64)  // movcf2gr rd, cj
+
+// 基于条件标志的分支指令：cj, offs21 (CF寄存器 + 21位偏移，使用R1I21_TYPE格式)
+`DEFINE_FP_INSTR(BCEQZ, R1I21_TYPE, BRANCH, LA64, IMM)  // bceqz cj, offs21
+`DEFINE_FP_INSTR(BCNEZ, R1I21_TYPE, BRANCH, LA64, IMM)  // bcnez cj, offs21
+
+// 浮点加载指令（立即数偏移）：fd, rj, si12 (FPR目标，GPR基址，12位立即数偏移，使用R2I12_TYPE格式)
+`DEFINE_FP_INSTR(FLD_S, R2I12_TYPE, LOAD, LA64, IMM)  // fld.s fd, rj, si12
+`DEFINE_FP_INSTR(FLD_D, R2I12_TYPE, LOAD, LA64, IMM)  // fld.d fd, rj, si12
+
+// 浮点存储指令（立即数偏移）：fd, rj, si12 (FPR源，GPR基址，12位立即数偏移，使用R2I12_TYPE格式)
+`DEFINE_FP_INSTR(FST_S, R2I12_TYPE, STORE, LA64, IMM)  // fst.s fd, rj, si12
+`DEFINE_FP_INSTR(FST_D, R2I12_TYPE, STORE, LA64, IMM)  // fst.d fd, rj, si12
+
+// 浮点加载指令（索引寻址）：fd, rj, rk (FPR目标，GPR基址，GPR索引，使用R3_TYPE格式)
+`DEFINE_FP_INSTR(FLDX_S, R3_TYPE, LOAD, LA64)  // fldx.s fd, rj, rk
+`DEFINE_FP_INSTR(FLDX_D, R3_TYPE, LOAD, LA64)  // fldx.d fd, rj, rk
+
+// 浮点存储指令（索引寻址）：fd, rj, rk (FPR源，GPR基址，GPR索引，使用R3_TYPE格式)
+`DEFINE_FP_INSTR(FSTX_S, R3_TYPE, STORE, LA64)  // fstx.s fd, rj, rk
+`DEFINE_FP_INSTR(FSTX_D, R3_TYPE, STORE, LA64)  // fstx.d fd, rj, rk
+
+// 浮点边界检查加载指令：fd, rj, rk (FPR目标，GPR基址，GPR边界，使用R3_TYPE格式)
+`DEFINE_FP_INSTR(FLDGT_S, R3_TYPE, LOAD, LA64)  // fldgt.s fd, rj, rk
+`DEFINE_FP_INSTR(FLDGT_D, R3_TYPE, LOAD, LA64)  // fldgt.d fd, rj, rk
+`DEFINE_FP_INSTR(FLDLE_S, R3_TYPE, LOAD, LA64)  // fldle.s fd, rj, rk
+`DEFINE_FP_INSTR(FLDLE_D, R3_TYPE, LOAD, LA64)  // fldle.d fd, rj, rk
+
+// 浮点边界检查存储指令：fd, rj, rk (FPR源，GPR基址，GPR边界，使用R3_TYPE格式)
+`DEFINE_FP_INSTR(FSTGT_S, R3_TYPE, STORE, LA64)  // fstgt.s fd, rj, rk
+`DEFINE_FP_INSTR(FSTGT_D, R3_TYPE, STORE, LA64)  // fstgt.d fd, rj, rk
+`DEFINE_FP_INSTR(FSTLE_S, R3_TYPE, STORE, LA64)  // fstle.s fd, rj, rk
+`DEFINE_FP_INSTR(FSTLE_D, R3_TYPE, STORE, LA64)  // fstle.d fd, rj, rk
+
+// 浮点比较指令：cd, fj, fk, cond (条件标志寄存器，2个浮点寄存器，条件码，使用R3_TYPE格式)
+// 注意：cond 是条件码（5位立即数，0x0-0x19），编码在指令中
+`DEFINE_FP_INSTR(FCMP_S, R3_TYPE, ARITHMETIC, LA64, IMM)  // fcmp.cond.s cd, fj, fk
+`DEFINE_FP_INSTR(FCMP_D, R3_TYPE, ARITHMETIC, LA64, IMM)  // fcmp.cond.d cd, fj, fk
--- src/isa/riscv_floating_point_instr.sv
+++ src/isa/riscv_floating_point_instr.sv
@@ -9,10 +9,14 @@ class riscv_floating_point_instr extends riscv_instr;
   rand riscv_fpr_t fs2;  // 浮点源寄存器2（对应 LoongArch 的 fk）
   rand riscv_fpr_t fs3;  // 浮点源寄存器3（对应 LoongArch 的 fa）
   rand riscv_fpr_t fd;   // 浮点目标寄存器（对应 LoongArch 的 fd）
+  rand riscv_fcsr_t fcsr;  // 浮点控制状态寄存器（FCSR0~FCSR3）
+  rand riscv_cfr_t cfr;    // 条件标志寄存器（FCC0~FCC7）
   bit              has_fs1 = 1'b1;
   bit              has_fs2 = 1'b1;
   bit              has_fs3 = 1'b0;
   bit              has_fd  = 1'b1;
+  bit              has_fcsr = 1'b0;  // 默认不使用FCSR
+  bit              has_cfr = 1'b0;   // 默认不使用CFR
 
   `uvm_object_utils(riscv_floating_point_instr)
   `uvm_object_new
@@ -24,17 +28,93 @@ class riscv_floating_point_instr extends riscv_instr;
     case (format)
       // LoongArch 浮点指令格式（与整数指令格式相同）
       R2_TYPE:
+        // GPR到FCSR移动指令：fcsr, rj (FCSR目标，GPR源)
+        if (instr_name == MOVGR2FCSR) begin
+          asm_str = $sformatf("%0s$fcsr%0d, $%0s", asm_str, fcsr, rs1.name());
+        end
+        // FCSR到GPR移动指令：rd, fcsr (GPR目标，FCSR源)
+        else if (instr_name == MOVFCSR2GR) begin
+          asm_str = $sformatf("%0s$%0s, $fcsr%0d", asm_str, rd.name(), fcsr);
+        end
+        // FPR到CF移动指令：cd, fj (CF目标，FPR源)
+        else if (instr_name == MOVFR2CF) begin
+          asm_str = $sformatf("%0s$fcc%0d, $%0s", asm_str, cfr, fs1.name());
+        end
+        // CF到FPR移动指令：fd, cj (FPR目标，CF源)
+        else if (instr_name == MOVCF2FR) begin
+          asm_str = $sformatf("%0s$%0s, $fcc%0d", asm_str, fd.name(), cfr);
+        end
+        // GPR到CF移动指令：cd, rj (CF目标，GPR源)
+        else if (instr_name == MOVGR2CF) begin
+          asm_str = $sformatf("%0s$fcc%0d, $%0s", asm_str, cfr, rs1.name());
+        end
+        // CF到GPR移动指令：rd, cj (GPR目标，CF源)
+        else if (instr_name == MOVCF2GR) begin
+          asm_str = $sformatf("%0s$%0s, $fcc%0d", asm_str, rd.name(), cfr);
+        end
+        // GPR到FPR移动指令：fd, rj (FPR目标，GPR源)
+        else if (instr_name inside {MOVGR2FR_W, MOVGR2FRH_W, MOVGR2FR_D}) begin
+          asm_str = $sformatf("%0s$%0s, $%0s", asm_str, fd.name(), rs1.name());
+        end
+        // FPR到GPR移动指令：rd, fj (GPR目标，FPR源)
+        else if (instr_name inside {MOVFR2GR_S, MOVFR2GR_D, MOVFRH2GR_S}) begin
+          asm_str = $sformatf("%0s$%0s, $%0s", asm_str, rd.name(), fs1.name());
+        end
         // LoongArch 浮点指令：fd, fj (2个浮点寄存器)
-        asm_str = $sformatf("%0s$%0s, $%0s", asm_str, fd.name(), fs1.name());
+        else begin
+          asm_str = $sformatf("%0s$%0s, $%0s", asm_str, fd.name(), fs1.name());
+        end
+      R1I21_TYPE:
+        // 基于条件标志的分支指令：cj, offs21 (CF寄存器 + 21位偏移)
+        if (instr_name inside {BCEQZ, BCNEZ}) begin
+          asm_str = $sformatf("%0s$fcc%0d, %0s", asm_str, cfr, get_imm());
+        end
+        else begin
+          // 其他R1I21_TYPE格式指令，调用基类处理
+          asm_str = super.convert2asm(prefix);
+        end
+      R2I12_TYPE:
+        // 浮点加载/存储指令（立即数偏移）：fd, rj, si12 (FPR，GPR基址，12位立即数)
+        if (instr_name inside {FLD_S, FLD_D, FST_S, FST_D}) begin
+          asm_str = $sformatf("%0s$%0s, $%0s, %0s", asm_str, fd.name(), rs1.name(), get_imm());
+        end
+        else begin
+          // 其他R2I12_TYPE格式指令，调用基类处理
+          asm_str = super.convert2asm(prefix);
+        end
       R3_TYPE:
+        // 浮点比较指令：cd, fj, fk, cond (条件标志寄存器，2个浮点寄存器，条件码)
+        if (instr_name inside {FCMP_S, FCMP_D}) begin
+          bit [4:0] cond_val = imm[4:0];  // cond 是5位立即数（0x0-0x19）
+          string cond_str = get_fcmp_cond_str(cond_val);
+          string precision = (instr_name == FCMP_S) ? "s" : "d";
+          asm_str = $sformatf("%0s.%0s.%0s $fcc%0d, $%0s, $%0s", asm_str, cond_str, precision, cfr, fs1.name(), fs2.name());
+        end
+        // 浮点加载/存储指令（索引寻址）：fd, rj, rk (FPR，GPR基址，GPR索引)
+        else if (instr_name inside {FLDX_S, FLDX_D, FSTX_S, FSTX_D}) begin
+          asm_str = $sformatf("%0s$%0s, $%0s, $%0s", asm_str, fd.name(), rs1.name(), rs2.name());
+        end
+        // 浮点边界检查加载/存储指令：fd, rj, rk (FPR，GPR基址，GPR边界)
+        else if (instr_name inside {FLDGT_S, FLDGT_D, FLDLE_S, FLDLE_D, FSTGT_S, FSTGT_D, FSTLE_S, FSTLE_D}) begin
+          asm_str = $sformatf("%0s$%0s, $%0s, $%0s", asm_str, fd.name(), rs1.name(), rs2.name());
+        end
         // LoongArch 浮点指令：fd, fj, fk (3个浮点寄存器)
         // 注意：LoongArch 使用 fj, fk 作为源寄存器，fd 作为目标寄存器
         // 在框架中，fs1 对应 fj，fs2 对应 fk，fd 对应 fd
-        asm_str = $sformatf("%0s$%0s, $%0s, $%0s", asm_str, fd.name(), fs1.name(), fs2.name());
+        else begin
+          asm_str = $sformatf("%0s$%0s, $%0s, $%0s", asm_str, fd.name(), fs1.name(), fs2.name());
+        end
       R4_TYPE:
-        // LoongArch 浮点指令：fd, fj, fk, fa (4个浮点寄存器)
-        // 在框架中，fs1 对应 fj，fs2 对应 fk，fs3 对应 fa，fd 对应 fd
-        asm_str = $sformatf("%0s$%0s, $%0s, $%0s, $%0s", asm_str, fd.name(), fs1.name(), fs2.name(), fs3.name());
+        // FSEL 指令：fd, fj, fk, ca (3个浮点寄存器 + 条件标志寄存器)
+        if (instr_name == FSEL) begin
+          // FSEL 指令格式：fsel fd, fj, fk, ca
+          // 在框架中，fd 对应 fd，fs1 对应 fj，fs2 对应 fk，ca 是条件标志寄存器（CFR）
+          asm_str = $sformatf("%0s$%0s, $%0s, $%0s, $fcc%0d", asm_str, fd.name(), fs1.name(), fs2.name(), cfr);
+        end else begin
+          // LoongArch 浮点指令：fd, fj, fk, fa (4个浮点寄存器)
+          // 在框架中，fs1 对应 fj，fs2 对应 fk，fs3 对应 fa，fd 对应 fd
+          asm_str = $sformatf("%0s$%0s, $%0s, $%0s, $%0s", asm_str, fd.name(), fs1.name(), fs2.name(), fs3.name());
+        end
       default:
         `uvm_fatal(`gfn, $sformatf("Unsupported LoongArch floating point format: %0s", format.name()))
     endcase
@@ -51,28 +131,202 @@ class riscv_floating_point_instr extends riscv_instr;
     case (format)
       // LoongArch 浮点指令格式（与整数指令格式相同）
       R2_TYPE: begin
+        // GPR到FCSR移动指令：fcsr, rj (FCSR目标，GPR源)
+        if (instr_name == MOVGR2FCSR) begin
+          has_rs1 = 1'b1;  // rj (GPR源)
+          has_rs2 = 1'b0;
+          has_rd  = 1'b0;
+          has_fs1 = 1'b0;
+          has_fs2 = 1'b0;
+          has_fd  = 1'b0;  // FCSR是特殊寄存器，不使用fd
+          has_fs3 = 1'b0;
+          has_fcsr = 1'b1;  // 使用FCSR寄存器
+          has_imm = 1'b0;
+        end
+        // FCSR到GPR移动指令：rd, fcsr (GPR目标，FCSR源)
+        else if (instr_name == MOVFCSR2GR) begin
+          has_rs1 = 1'b0;
+          has_rs2 = 1'b0;
+          has_rd  = 1'b1;  // rd (GPR目标)
+          has_fs1 = 1'b0;  // FCSR是特殊寄存器，不使用fs1
+          has_fs2 = 1'b0;
+          has_fd  = 1'b0;
+          has_fs3 = 1'b0;
+          has_fcsr = 1'b1;  // 使用FCSR寄存器
+          has_imm = 1'b0;
+        end
+        // FPR到CF移动指令：cd, fj (CF目标，FPR源)
+        else if (instr_name == MOVFR2CF) begin
+          has_rs1 = 1'b0;
+          has_rs2 = 1'b0;
+          has_rd  = 1'b0;  // 不使用rd，使用cfr字段
+          has_fs1 = 1'b1;  // fj (FPR源)
+          has_fs2 = 1'b0;
+          has_fd  = 1'b0;
+          has_fs3 = 1'b0;
+          has_cfr = 1'b1;  // 使用CFR寄存器
+          has_imm = 1'b0;
+        end
+        // CF到FPR移动指令：fd, cj (FPR目标，CF源)
+        else if (instr_name == MOVCF2FR) begin
+          has_rs1 = 1'b0;  // 不使用rs1，使用cfr字段
+          has_rs2 = 1'b0;
+          has_rd  = 1'b0;
+          has_fs1 = 1'b0;
+          has_fs2 = 1'b0;
+          has_fd  = 1'b1;  // fd (FPR目标)
+          has_fs3 = 1'b0;
+          has_cfr = 1'b1;  // 使用CFR寄存器
+          has_imm = 1'b0;
+        end
+        // GPR到CF移动指令：cd, rj (CF目标，GPR源)
+        else if (instr_name == MOVGR2CF) begin
+          has_rs1 = 1'b1;  // rj (GPR源)
+          has_rs2 = 1'b0;
+          has_rd  = 1'b0;  // 不使用rd，使用cfr字段
+          has_fs1 = 1'b0;
+          has_fs2 = 1'b0;
+          has_fd  = 1'b0;
+          has_fs3 = 1'b0;
+          has_cfr = 1'b1;  // 使用CFR寄存器
+          has_imm = 1'b0;
+        end
+        // CF到GPR移动指令：rd, cj (GPR目标，CF源)
+        else if (instr_name == MOVCF2GR) begin
+          has_rs1 = 1'b0;  // 不使用rs1，使用cfr字段
+          has_rs2 = 1'b0;
+          has_rd  = 1'b1;  // rd (GPR目标)
+          has_fs1 = 1'b0;
+          has_fs2 = 1'b0;
+          has_fd  = 1'b0;
+          has_fs3 = 1'b0;
+          has_cfr = 1'b1;  // 使用CFR寄存器
+          has_imm = 1'b0;
+        end
+        // GPR到FPR移动指令：fd, rj (FPR目标，GPR源)
+        else if (instr_name inside {MOVGR2FR_W, MOVGR2FRH_W, MOVGR2FR_D}) begin
+          has_rs1 = 1'b1;  // rj (GPR源)
+          has_rs2 = 1'b0;
+          has_rd  = 1'b0;
+          has_fs1 = 1'b0;
+          has_fs2 = 1'b0;
+          has_fd  = 1'b1;  // fd (FPR目标)
+          has_fs3 = 1'b0;
+          has_imm = 1'b0;
+        end
+        // FPR到GPR移动指令：rd, fj (GPR目标，FPR源)
+        else if (instr_name inside {MOVFR2GR_S, MOVFR2GR_D, MOVFRH2GR_S}) begin
+          has_rs1 = 1'b0;
+          has_rs2 = 1'b0;
+          has_rd  = 1'b1;  // rd (GPR目标)
+          has_fs1 = 1'b1;  // fj (FPR源)
+          has_fs2 = 1'b0;
+          has_fd  = 1'b0;
+          has_fs3 = 1'b0;
+          has_imm = 1'b0;
+        end
         // LoongArch 浮点指令：fd, fj (2个浮点寄存器)
-        has_fs1 = 1'b1;  // fj
-        has_fs2 = 1'b0;
-        has_fd  = 1'b1;  // fd
-        has_fs3 = 1'b0;
-        has_imm = 1'b0;
+        else begin
+          has_fs1 = 1'b1;  // fj
+          has_fs2 = 1'b0;
+          has_fd  = 1'b1;  // fd
+          has_fs3 = 1'b0;
+          has_imm = 1'b0;
+        end
+      end
+      R2I12_TYPE: begin
+        // 浮点加载/存储指令（立即数偏移）：fd, rj, si12 (FPR，GPR基址，12位立即数)
+        if (instr_name inside {FLD_S, FLD_D, FST_S, FST_D}) begin
+          has_rs1 = 1'b1;  // rj (GPR基址)
+          has_rs2 = 1'b0;
+          has_rd  = 1'b0;
+          has_fs1 = 1'b0;
+          has_fs2 = 1'b0;
+          has_fd  = 1'b1;  // fd (FPR)
+          has_fs3 = 1'b0;
+          has_imm = 1'b1;  // 12位立即数偏移
+        end
+        else begin
+          // 其他R2I12_TYPE格式指令，调用基类处理
+          super.set_rand_mode();
+        end
       end
       R3_TYPE: begin
+        // 浮点比较指令：cd, fj, fk, cond (条件标志寄存器，2个浮点寄存器，条件码)
+        if (instr_name inside {FCMP_S, FCMP_D}) begin
+          has_fs1 = 1'b1;  // fj
+          has_fs2 = 1'b1;  // fk
+          has_fd  = 1'b0;  // 不使用fd，使用cfr
+          has_fs3 = 1'b0;
+          has_cfr = 1'b1;  // cd (条件标志寄存器)
+          has_imm = 1'b1;  // cond (条件码，5位)
+        end
+        // 浮点加载/存储指令（索引寻址）：fd, rj, rk (FPR，GPR基址，GPR索引)
+        else if (instr_name inside {FLDX_S, FLDX_D, FSTX_S, FSTX_D}) begin
+          has_rs1 = 1'b1;  // rj (GPR基址)
+          has_rs2 = 1'b1;  // rk (GPR索引)
+          has_rd  = 1'b0;
+          has_fs1 = 1'b0;
+          has_fs2 = 1'b0;
+          has_fd  = 1'b1;  // fd (FPR)
+          has_fs3 = 1'b0;
+          has_imm = 1'b0;
+        end
+        // 浮点边界检查加载/存储指令：fd, rj, rk (FPR，GPR基址，GPR边界)
+        else if (instr_name inside {FLDGT_S, FLDGT_D, FLDLE_S, FLDLE_D, FSTGT_S, FSTGT_D, FSTLE_S, FSTLE_D}) begin
+          has_rs1 = 1'b1;  // rj (GPR基址)
+          has_rs2 = 1'b1;  // rk (GPR边界)
+          has_rd  = 1'b0;
+          has_fs1 = 1'b0;
+          has_fs2 = 1'b0;
+          has_fd  = 1'b1;  // fd (FPR)
+          has_fs3 = 1'b0;
+          has_imm = 1'b0;
+        end
         // LoongArch 浮点指令：fd, fj, fk (3个浮点寄存器)
-        has_fs1 = 1'b1;  // fj
-        has_fs2 = 1'b1;  // fk
-        has_fd  = 1'b1;  // fd
-        has_fs3 = 1'b0;
-        has_imm = 1'b0;
+        else begin
+          has_fs1 = 1'b1;  // fj
+          has_fs2 = 1'b1;  // fk
+          has_fd  = 1'b1;  // fd
+          has_fs3 = 1'b0;
+          has_imm = 1'b0;
+        end
       end
       R4_TYPE: begin
-        // LoongArch 浮点指令：fd, fj, fk, fa (4个浮点寄存器)
-        has_fs1 = 1'b1;  // fj
-        has_fs2 = 1'b1;  // fk
-        has_fs3 = 1'b1;  // fa
-        has_fd  = 1'b1;  // fd
-        has_imm = 1'b0;
+        // FSEL 指令：fd, fj, fk, ca (3个浮点寄存器 + 条件标志寄存器)
+        if (instr_name == FSEL) begin
+          has_fs1 = 1'b1;  // fj
+          has_fs2 = 1'b1;  // fk
+          has_fd  = 1'b1;  // fd
+          has_fs3 = 1'b0;  // 不使用 fs3
+          has_imm = 1'b0;  // 不使用立即数
+          has_cfr = 1'b1;  // ca 是条件标志寄存器（CFR）
+        end else begin
+          // LoongArch 浮点指令：fd, fj, fk, fa (4个浮点寄存器)
+          has_fs1 = 1'b1;  // fj
+          has_fs2 = 1'b1;  // fk
+          has_fs3 = 1'b1;  // fa
+          has_fd  = 1'b1;  // fd
+          has_imm = 1'b0;
+        end
+      end
+      R1I21_TYPE: begin
+        // 基于条件标志的分支指令：cj, offs21 (CF寄存器 + 21位偏移)
+        if (instr_name inside {BCEQZ, BCNEZ}) begin
+          has_rs1 = 1'b0;  // 不使用rs1，使用cfr字段
+          has_rs2 = 1'b0;
+          has_rd  = 1'b0;
+          has_fs1 = 1'b0;
+          has_fs2 = 1'b0;
+          has_fd  = 1'b0;
+          has_fs3 = 1'b0;
+          has_cfr = 1'b1;  // 使用CFR寄存器
+          has_imm = 1'b1;  // 21位偏移量
+        end
+        else begin
+          // 其他R1I21_TYPE格式指令，调用基类处理
+          super.set_rand_mode();
+        end
       end
       default: `uvm_info(`gfn, $sformatf("Unsupported LoongArch floating point format %0s", format.name()), UVM_LOW)
     endcase
@@ -80,10 +334,15 @@ class riscv_floating_point_instr extends riscv_instr;
 
   function void pre_randomize();
     super.pre_randomize();
+    // For GPR-to-FPR move instructions, rs1 (GPR) is used
+    // For FPR-to-GPR move instructions, rd (GPR) is used
+    // These are already handled in set_rand_mode() and super.pre_randomize()
     fs1.rand_mode(has_fs1);
     fs2.rand_mode(has_fs2);
     fs3.rand_mode(has_fs3);
     fd.rand_mode(has_fd);
+    fcsr.rand_mode(has_fcsr);
+    cfr.rand_mode(has_cfr);
   endfunction
 
   virtual function void do_copy(uvm_object rhs);
@@ -94,16 +353,75 @@ class riscv_floating_point_instr extends riscv_instr;
     this.fs2     = rhs_.fs2;
     this.fs1     = rhs_.fs1;
     this.fd      = rhs_.fd;
+    this.fcsr    = rhs_.fcsr;
+    this.cfr     = rhs_.cfr;
     this.has_fs3 = rhs_.has_fs3;
     this.has_fs2 = rhs_.has_fs2;
     this.has_fs1 = rhs_.has_fs1;
     this.has_fd  = rhs_.has_fd;
+    this.has_fcsr = rhs_.has_fcsr;
+    this.has_cfr = rhs_.has_cfr;
   endfunction : do_copy
 
   virtual function void set_imm_len();
-    // LoongArch 浮点指令的立即数长度由格式决定，在基类 riscv_instr 中已处理
-    // 这里可以添加浮点指令特定的立即数长度设置
+    if (instr_name inside {BCEQZ, BCNEZ}) begin
+      // BCEQZ/BCNEZ指令：21位偏移量
+      imm_len = 21;
+    end else if (instr_name inside {FCMP_S, FCMP_D}) begin
+      // FCMP指令：cond 是5位立即数（0x0-0x19）
+      imm_len = 5;
+    end else begin
+      // LoongArch 浮点指令的立即数长度由格式决定，在基类 riscv_instr 中已处理
+      // 这里可以添加浮点指令特定的立即数长度设置
+    end
   endfunction: set_imm_len
+
+  // 将FCMP指令的条件码转换为助记符字符串
+  function string get_fcmp_cond_str(bit [4:0] cond_val);
+    case (cond_val)
+      5'h00: return "caf";
+      5'h01: return "saf";
+      5'h02: return "clt";
+      5'h03: return "slt";
+      5'h04: return "ceq";
+      5'h05: return "seq";
+      5'h06: return "cle";
+      5'h07: return "sle";
+      5'h08: return "cun";
+      5'h09: return "sun";
+      5'h0A: return "cult";
+      5'h0B: return "sult";
+      5'h0C: return "cueq";
+      5'h0D: return "sueq";
+      5'h0E: return "cule";
+      5'h0F: return "sule";
+      5'h10: return "cne";
+      5'h11: return "sne";
+      5'h14: return "cor";
+      5'h15: return "sor";
+      5'h18: return "cune";
+      5'h19: return "sune";
+      default: return $sformatf("cond%0d", cond_val);
+    endcase
+  endfunction: get_fcmp_cond_str
+
+  // 基于条件标志的分支指令约束
+  constraint bceqz_bcnez_c {
+    if (instr_name inside {BCEQZ, BCNEZ})
+      // 分支偏移量按字对齐，低2位必须为0
+      imm[1:0] == 2'b00;
+  }
+
+  // FCMP指令约束：cond 值必须在有效范围内（0x0-0x19，但某些值无效）
+  constraint fcmp_cond_c {
+    if (instr_name inside {FCMP_S, FCMP_D}) {
+      // 有效的 cond 值：0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x10, 0x11, 0x14, 0x15, 0x18, 0x19
+      imm[4:0] inside {5'h00, 5'h01, 5'h02, 5'h03, 5'h04, 5'h05, 5'h06, 5'h07, 
+                        5'h08, 5'h09, 5'h0A, 5'h0B, 5'h0C, 5'h0D, 5'h0E, 5'h0F,
+                        5'h10, 5'h11, 5'h14, 5'h15, 5'h18, 5'h19};
+      imm[XLEN-1:5] == 0;  // 高位必须为0
+    }
+  }
 
   // coverage related functions - 暂时注释掉
   /*
